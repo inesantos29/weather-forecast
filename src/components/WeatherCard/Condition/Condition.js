aa803d8864cff1b4a6b25ea6c2c3eab39b8c4db0
inesantos29 <ines.arts@hotmail.com>
2020-03-22 19:39:14 +0000

    Update Condition component

--- src/components/WeatherCard/Condition/Condition.js
+++ src/components/WeatherCard/Condition/Condition.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import {Temperature, State} from './style';
+import { Temperature, Status } from './style';
 
 const Condition = ({temp, temp_min, temp_max, description}) => {
 
-  const minMaxTemp = (min, max) => (
-    <h4>
-      <span>{min}&deg;</span>
-      <span>{max}&deg;</span>
-    </h4>
-  );
+  const minMaxTemp = (min, max) => {
+    if (min && max) {
+      return (
+        <h3>
+          <span className="m-right">Min {min}&deg;</span>
+          <span>Max {max}&deg;</span>
+        </h3>
+      )
+    }
+  };
 
   return(
     <>
       <Temperature>{temp}&deg;</Temperature>
       {minMaxTemp(temp_min, temp_max)}
-      <State>{description}</State>
+      <Status>{description}</Status>
     </>
   );
 };
--- src/components/WeatherCard/Condition/style.js
+++ src/components/WeatherCard/Condition/style.js
@@ -1,10 +1,11 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Temperature = styled.h2`
     font-size: 2rem;
     font-weight:200;
   `;
 
-export const State = styled.h3`
+export const Status = styled.h3`
     font-size: 1.2rem;
+    text-transform: capitalize;
   `;
